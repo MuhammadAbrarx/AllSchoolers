a296decdcb80303986fc4ee7e3e9c6692c72e92b
Md. Abrar <Muhammadabrarx@gmail.com>
2017-12-13 19:18:53 +0600

    still not a stable master 0.01

---
+++ app/src/main/java/com/abdullahsoft/allschoolers/Activity/CoreActivity.java
@@ -0,0 +1,15 @@
+package com.abdullahsoft.allschoolers.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.abdullahsoft.allschoolers.R;
+
+public class CoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core);
+    }
+}
